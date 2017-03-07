f4c82344abf57faad43f824c7a2018a29cba3b80
Grandure <442792829@qq.com>
2017-03-07 10:25:20 +0800

    定义私有模块用于存/读数据库中的内容

--- js/index.js
+++ js/index.js
@@ -1,88 +1,101 @@
-    function searchPage(setPage) { //查询页面 若输入参数为设置页面，若参数为空为查询页面
-        var getPage = document.getElementById('my-page');
-        currentPage = parseInt(getPage.value);
-        if (arguments.length == 0) {
-            return currentPage
-        }
-        currentPage = setPage;
-        return currentPage;
+function searchPage(setPage) { //查询页面 若输入参数为设置页面，若参数为空为查询页面
+    var getPage = document.getElementById('my-page');
+    currentPage = parseInt(getPage.value);
+    if (arguments.length == 0) {
+        return currentPage
     }
+    currentPage = setPage;
+    return currentPage;
+}
 
-    function compusePage(itemTotal) { //itemTotal表示数据库内容总数
-        var pageNum = searchPage(); //从Ajax获取数据库内容 searchPage参数需要增加获取的值;
-        var result = itemTotal / pageNum;
-        //searchPage(); //查询当前设置页数
-        if (itemTotal % pageNum !== 0) {
-            result = result + 1;
-        }
-        if (result <= 1) {
-            pageNum = 1;
-        }
-        pageNum = parseInt(result, 10);
-        console.log('当前分页数为:' + '' + pageNum);
+function compusePage(itemTotal) { //itemTotal表示数据库内容总数
+    var pageNum = searchPage(); //从Ajax获取数据库内容 searchPage参数需要增加获取的值;
+    var result = itemTotal / pageNum;
+    //searchPage(); //查询当前设置页数
+    if (itemTotal % pageNum !== 0) {
+        result = result + 1;
     }
+    if (result <= 1) {
+        pageNum = 1;
+    }
+    pageNum = parseInt(result, 10);
+    console.log('当前分页数为:' + '' + pageNum);
+}
+var myModul = (function(m) { //创建一个模块用于存储数据库的内容
+    var f = {};
+    var Fnum;
+    f.getnum = function() {
+        // console.log(Fnum);
+        return Fnum
+    };
+    f.setnum = function(n) {
+        Fnum = n;
+    };
+    return f;
+}());
 
-    function showPage() {}
-    $(document).ready(function() {
-        function list(Pages, res) { //获取数据库内容并显示出来
-            var _list = ""; //存储列表临时变量
-            var _page = ""; //存储页面临时变量
-            var compuse;
-            for (var i = 0; i < Pages; i++) {
-                _list += "<tr><td>" + "<input type='checkbox'/>" + "<span>" + res[i]['id'] + "</span>" + "\
+function showPage() {}
+$(document).ready(function() {
+    function list(Pages, res) { //获取数据库内容并显示出来
+        var _list = ""; //存储列表临时变量
+        var _page = ""; //存储页面临时变量
+        var compuse;
+        for (var i = 0; i < Pages; i++) {
+            _list += "<tr><td>" + "<input type='checkbox'/>" + "<span>" + res[i]['id'] + "</span>" + "\
                     </td>/<td>" + res[i]['book'] + "</td>\
                     <td>" + res[i]['code'] + "</td>\
                     <td>" + res[i]['borrower'] + "</td>\
                     <td>" + res[i]['tel'] + "</td>\
                     <td>" + res[i]['more'] + "</td>\</tr>";
 
-            }
-            if (res.length % Pages != 0) {
-                compuse = res.length / Pages + 1;
-            } else {
-                compuse = res.length / Pages
-            }
-            if (res.length <= 1) {
-                compuse = 1;
-            }
-            compuse = parseInt(compuse, 10);
-            for (var i = 1; i <= compuse; i++) {
-                $('#Nex').parent().before('<li><a href="">' + i + '</a></li>');
-            }
-            console.log('当前数据页数为:' + ' ' + compuse);
-            (function() {
-
-                $('#list-book').html(_list);
-
-                $('#item-total').html('共含有' + "<span id='dataNumber'>" + res.length + '</span>' + ' ' + '条数据');
-            })();
-            $('#Pre').after(_page);
         }
-        function loaddata() {
-            $.ajax({
-                url: 'data/index.php',
-                type: 'get',
-                dataType: 'json',
-                data: {},
-                success: function(res) {
-                    // alert('数据导入成功!')
-                    console.log('数据载入成功!');
-                    var Pages = searchPage(); //获取当前页数
-                    list(Pages, res); //显示数据库
-                },
-                error: function() {
-                    alert("数据导入失败，请检查数据库是否正确连接或index.html文件是否在服务器下运行");
-                }
-            });
+        if (res.length % Pages != 0) {
+            compuse = res.length / Pages + 1;
+        } else {
+            compuse = res.length / Pages
         }
-        loaddata();
-        $(document).ready(function() {
-            $('tbody').on('click', 'tr', function() {
-                $('tr>th').addClass('bg-info');
-                $('tr:even').addClass('success');
-            });
-            setTimeout(function() {
-                $('tr').trigger('click')
-            }, 10);
+        if (res.length <= 1) {
+            compuse = 1;
+        }
+        compuse = parseInt(compuse, 10);
+        for (var i = 1; i <= compuse; i++) {
+            $('#Nex').parent().before('<li><a href="">' + i + '</a></li>');
+        }
+        console.log('当前数据页数为:' + ' ' + compuse);
+        (function() {
+
+            $('#list-book').html(_list);
+
+            $('#item-total').html('共含有' + "<span id='dataNumber'>" + res.length + '</span>' + ' ' + '条数据');
+        })();
+        $('#Pre').after(_page);
+    }
+
+    function loaddata() {
+        $.ajax({
+            url: 'data/index.php',
+            type: 'get',
+            dataType: 'json',
+            data: {},
+            success: function(res) {
+                // alert('数据导入成功!')
+                console.log('数据载入成功!');
+                var Pages = searchPage(); //获取当前页数
+                list(Pages, res); //显示数据库
+            },
+            error: function() {
+                alert("数据导入失败，请检查数据库是否正确连接或index.html文件是否在服务器下运行");
+            }
         });
+    }
+    loaddata();
+    $(document).ready(function() {
+        $('tbody').on('click', 'tr', function() {
+            $('tr>th').addClass('bg-info');
+            $('tr:even').addClass('success');
+        });
+        setTimeout(function() {
+            $('tr').trigger('click')
+        }, 10);
     });
+});
